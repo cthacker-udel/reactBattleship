57ec2e69c04d4b5cc715f0b44465acb6eccbc03e
cthacker-udel <cthacker@udel.edu>
2021-09-23 23:16:51 -0400

    Updated source files

--- src/index.js
+++ src/index.js
@@ -10,20 +10,31 @@ import Badge from 'react-bootstrap/Badge'
 
 function Board(props){
 
-    const [theGuesses,setTestState] = useState(props.theCompGuesses);
+    const [theGuesses,setTheGuesses] = useState(props.theCompGuesses);
 
     const [theColor,setColor] = useState('outline-primary');
 
     const printOut = (event) => {
 
         //console.log(event.target.name); // acquire button name that is pressed
-
+        console.log(`guessesArr = ${props.guessesArr}`);
         let guess = event.target.name;
         console.log(`guesses2 = ${theGuesses}`);
         console.log(`the hits arr = ${props.theHitsArr}`);
-        if(props.theHitsArr.length === theGuesses.length){
-            console.log(`setting buttons to success`);
-            setColor("success");
+        if(props.theHitsArr.length === theGuesses.length-1 && !props.theHitsArr.includes(event.target.name) && theGuesses.includes(event.target.name)){
+            setColor("outline-primary");
+            let currWins = props.numWins+1;
+            props.updateWins(currWins);
+            props.guessesArr.push(event.target.name);
+            for(let eachname of props.guessesArr){
+
+                document.getElementById(eachname).className = "btn btn-outline-primary";
+                document.getElementById(eachname).innerHTML = eachname;
+
+            }
+            props.setGuessesArr([]);
+            props.winProcedure(setTheGuesses);
+            return;
         }
         else if(props.theHitsArr.includes(guess)){
             // has already been hit
@@ -33,6 +44,7 @@ function Board(props){
             // valid hit
             console.log(`hit!`);
             document.getElementById(guess).innerHTML = "HIT";
+            document.getElementById(guess).className = "btn btn-success";
             props.updateHits(props.numHits+1);
             props.updateHitArr(props.theHitsArr.concat(guess));
             console.log(`the hits arr = ${props.theHitsArr}`);
@@ -40,12 +52,15 @@ function Board(props){
         else{
             console.log(`miss!`);
             document.getElementById(guess).innerHTML = "MISS";
+            document.getElementById(guess).className = "btn btn-danger";
             props.updateMisses(props.numMisses+1);
         }
+        props.guessesArr.push(event.target.name);
         
     }
 
     const generateSpace = (theName) => {
+
 
         return(
 
@@ -214,7 +229,41 @@ function MainPage(){
     const theChoices = [['A1','B1','C1','D1','E1'],['A2','B2','C2','D2','E2'],['A3','B3','C3','D3','E3'],['A4','B4','C4','D4','E4'],['A5','B5','C5','D5','E5']];
 
     const [options,setOptions] = useState(theChoices);
-    const [turn,setTurn] = useState(true); // user goes first if true
+
+    const getCompGuesses = () => {
+
+        let randNums = [];
+        let x = 0;
+        let y = 0;
+        let fndPair = false;
+        do{
+            x = Math.floor(Math.random() *(5));
+            y = Math.floor(Math.random() *(5));
+            for(let i = 0; i < randNums.length; i++){
+                let eachpair = randNums[i];
+                if(eachpair[0] === x && eachpair[1] === y){
+                    fndPair = true;
+                    break;
+                }
+                else{
+                    fndPair = false;
+                }
+            }
+            if(fndPair){
+                continue;
+            }
+            else{
+                randNums.push([x,y]);
+            }
+        }while(randNums.length !== 5);
+    
+        let theGuesses = [];
+        for(let eachpair of randNums){
+            theGuesses.push(options[eachpair[0]][eachpair[1]]);
+        }
+        return theGuesses;
+    
+    }
 
     // get computer guesses
 
@@ -225,52 +274,60 @@ function MainPage(){
     // really odd behavior when using random numbers, they would change from here to being passed to the board, unsure why, and then when I called set hits in the board func,
     // it would edit the random numbers as well
 
-    const compGuess = [options[Math.floor(Math.random() *(5))][Math.floor(Math.random() *(5))]
-                        ,options[Math.floor(Math.random() *(5))][Math.floor(Math.random() *(5))]
-                        ,options[Math.floor(Math.random() *(5))][Math.floor(Math.random() *(5))]
-                        ,options[Math.floor(Math.random() *(5))][Math.floor(Math.random() *(5))]
-                        ,options[Math.floor(Math.random() *(5))][Math.floor(Math.random() *(5))]
-                    ];
+    const postWin = (setBoardGuesses) => {
+
+        let guesses = getCompGuesses();
+        setComputerGuess(guesses);
+        setHitsArr([]);
+        setBoardGuesses(guesses);
+
+    }
+
+    const compGuess = getCompGuesses();
 
     const[computerGuess,setComputerGuess] = useState(compGuess);
 
     const [misses,setMisses] = useState(0);
     const [hits,setHits] = useState(0);
-    const [sank,setSank] = useState(0);
+    const [wins,setWins] = useState(0);
+
+    console.log(`wins = ${wins}`);
 
     const [hitsArr,setHitsArr] = useState([]);
 
-    console.log(`compguess = ${compGuess}`);
+    const [guesses,setGuesses] = useState([]);
+
+    //console.log(`compguess = ${compGuess}`);
 
     console.log(`hits = ${hits}`);
 
     return(
         <>
+            <Row style={{textAlign: "center"}}>
+                <Col>
+
+                    <h3><Badge bg="primary" id="missesBadge">Misses : {misses}</Badge></h3>
+
+                </Col>
+                <Col>
+
+                    <h3><Badge bg="primary" id="hitsbadge">Hits : {hits}</Badge></h3>
+
+                </Col>
+                <Col>
+
+                    <h3><Badge bg="primary" id="sankBadge">Wins : {wins}</Badge></h3>
+
+                </Col>
+            </Row>
             <Row>
 
                 <Col>
 
-                <Board theOptions={options} theCompGuesses={computerGuess} updateHits={setHits} updateSank={setSank} updateMisses={setMisses} numHits={hits} numMisses={misses} numSank={sank} updateHitArr={setHitsArr} theHitsArr={hitsArr}/>
+                <Board theOptions={options} theCompGuesses={computerGuess} updateHits={setHits} updateWins={setWins} updateMisses={setMisses} numHits={hits} numMisses={misses} numWins={wins} updateHitArr={setHitsArr} theHitsArr={hitsArr} winProcedure={postWin} guessesArr={guesses} setGuessesArr={setGuesses}/>
 
                 </Col>
 
-            </Row>
-            <Row style={{textAlign: "center"}}>
-                <Col>
-
-                    <Badge bg="primary" id="missesBadge">Misses : {misses}</Badge>
-
-                </Col>
-                <Col>
-
-                    <Badge bg="primary" id="hitsbadge">Hits : {hits}</Badge>
-
-                </Col>
-                <Col>
-
-                    <Badge bg="primary" id="sankBadge">[TO BE IMPLEMENTED]Sank : {sank}</Badge>
-
-                </Col>
             </Row>
             <br />
             <br />
